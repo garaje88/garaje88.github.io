bb6ab825bc9f24f128c180a7b5efeeacb454f719
Carlos Polanco <carlos.polanco010@gmail.com>
2024-04-22 19:36:22 -0500

    feat: new image and modification page main

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -21,20 +21,20 @@ const servicesSkoovify = [
 
 const servicesGaraje88 = [
     {
-        title: "Post",
-        description: "In Skivoofy, users can freely and creatively create text and image posts.",
+        title: "Our Expertise",
+        description: "We design and develop custom web applications with a focus on seamless user experience, scalability, and performance. Our expertise in Angular and Astro ensures your web presence is modern, responsive, and effective.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "AI",
-        description: "The platform uses artificial intelligence to analyze users' posts and provide intelligent suggestions for enhancing them.",
+        title: "Enterprise Backend Solutions",
+        description: "Secure, scalable, and robust backend solutions crafted using Java and Spring Boot. We specialize in creating powerful backends that are the backbone of enterprise-grade applications.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Suggest",
-        description: "Skivoofy's AI can suggest changes to the text, such as grammatical corrections, tone or writing style optimization, and offer ideas to make posts more engaging and captivating.",
+        title: "Technology Consulting & Strategy",
+        description: "Leverage our deep industry knowledge and technical proficiency to formulate strategies that align with your business goals. We offer consultancy that helps you navigate the complexities of digital transformation.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     }
